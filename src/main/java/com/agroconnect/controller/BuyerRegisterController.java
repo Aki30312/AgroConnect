1a284c15189f3765e16dd9038be3ac9a1ef879dc
Eleni Gebremedhin <elenigebremedhin07@gmail.com>
2026-02-01 03:20:21 +0300

    Connected BuyerUser with DAO

--- src/main/java/com/agroconnect/controller/BuyerRegisterController.java
+++ src/main/java/com/agroconnect/controller/BuyerRegisterController.java
@@ -1,5 +1,7 @@
 package com.agroconnect.controller;
 
+import com.agroconnect.dao.BuyerDAO;
+import com.agroconnect.model.BuyerUser;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -13,14 +15,41 @@ public class BuyerRegisterController {
     @FXML private TextField txtPhone;
     @FXML private Label lblStatus;
 
+    private final BuyerDAO buyerDAO = new BuyerDAO();
+
     @FXML
     private void handleRegister() {
-        lblStatus.setText("Register Buyer clicked (connect DAO later).");
+        String name = txtName.getText().trim();
+        String email = txtEmail.getText().trim();
+        String password = txtPassword.getText();
+        String phone = txtPhone.getText().trim();
+
+        if (name.isEmpty()  email.isEmpty()  password.isEmpty()) {
+            lblStatus.setText("Name, Email, and Password are required.");
+            return;
+        }
+
+        BuyerUser buyer = new BuyerUser(name, email, password, phone);
+
+        try {
+            int newId = buyerDAO.registerBuyer(buyer);
+
+            if (newId > 0) {
+                lblStatus.setText("Buyer registered successfully! ID: " + newId);
+                MainSceneController.getInstance().switchView("login.fxml");
+            } else {
+                lblStatus.setText("Registration failed.");
+            }
+
+        } catch (Exception e) {
+            // Most common: UNIQUE constraint failed: buyers.email
+            lblStatus.setText("Registration error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void goBackToLogin() {
         MainSceneController.getInstance().switchView("login.fxml");
     }
-
 }
